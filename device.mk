8141aed0e8498c24b65a0d150dffab8f3994eae6
Patrick Tjin <pattjin@google.com>
2019-10-15 10:32:37 +0800

    lavender: Add nosdcard characteristic to device
    * Just as per stock.
    
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff

--- device.mk
+++ device.mk
@@ -10,6 +10,8 @@ $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
