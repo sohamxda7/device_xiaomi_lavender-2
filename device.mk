4456dcc78b850febd7a08aa9ccc4a87b8bd7bbde
faham1997 <nafidfaham08@gmail.com>
2020-10-23 06:28:48 +0600

    Revert "lavender : Use Pixel Charger Animation"
    This reverts commit 17f3ec50fc5cf6c6cd29eaa4fc845207d8fe85d9.

--- device.mk
+++ device.mk
@@ -263,7 +263,9 @@ PRODUCT_COPY_FILES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
+
 
 # IFAA manager
 PRODUCT_PACKAGES += \
