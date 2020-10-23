cc670ef689beb395cba7c5c8892ac07ead7a7813
faham1997 <nafidfaham08@gmail.com>
2020-10-23 06:27:13 +0600

    lavender : Adapt overlays for lineageOS

--- device.mk
+++ device.mk
@@ -18,11 +18,13 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-system
 
 # RRO configuration
-PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Set boot SPL
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
