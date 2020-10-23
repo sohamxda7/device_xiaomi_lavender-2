7c27169f5c6c35c4d97cf6b508d429825230c0ba
faham1997 <nafidfaham08@gmail.com>
2020-10-23 06:13:54 +0600

    lavender : Initial Bringup For LineageOS
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>

--- device.mk
+++ device.mk
@@ -21,11 +21,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe \
     $(LOCAL_PATH)/overlay-system
 
 # RRO configuration
-TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
